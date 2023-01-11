a0d233f8a7788033df8bacfe89c0d5d70f3e7007
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-11 15:03:55 -0800

    create a function to format text output

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,11 +3,26 @@ import classNames from "classnames"
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  let liClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0});
+  let liClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": (props.spots === 0)});
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return "no spots remaining"
+    }
+
+    if (props.spots === 1) {
+      return "1 spot remaining"
+    }
+
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`
+    }
+  };
+
   return (
     <li onClick={() => props.setDay(props.name)} className={liClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
