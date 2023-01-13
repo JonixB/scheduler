1dc4a8b3da93847ffb2531c4304625b06bb5b032
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-13 13:56:37 -0800

    refactor getAppointmentsForDay selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,20 +1,19 @@
 export function getAppointmentsForDay(state, day) {
-  const filteredDays = state.days.filter(stateDay => stateDay.name === day)
-  const result = [];
 
-  if (filteredDays.length < 1) {
-    return result;
+  if (!state.days.find(stateDay => stateDay.name === day)) {
+    return [];
   }
+
+  const filteredDay = state.days.find(stateDay => stateDay.name === day)
 
   if (state.days.length < 1) {
-    return result;
+    return [];
   }
 
-  //loops through the appointment array in the filteredday and matches the id in appointment object
-  for (let x = 0; x < filteredDays[0].appointments.length; x++) {
-    if ((filteredDays[0].appointments[x]) in state.appointments) {
-      result.push(state.appointments[filteredDays[0].appointments[x]]);
+  const result = filteredDay.appointments.map((appointment) => {
+    if (appointment in state.appointments) {
+      return state.appointments[appointment];
     }
-  }
+  });
   return result;
 }
