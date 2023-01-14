a3a2b137add10327d8559d7b249b0d04c8bd9626
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-13 19:35:09 -0800

    add interviewer state and request

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,7 +10,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewer: {}
   });
 
   let dailyAppointments = [];
@@ -20,9 +21,10 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get('api/days'),
-      axios.get('api/appointments')
+      axios.get('api/appointments'),
+      axios.get('api/interviewers')
     ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewer: all[2] }));
     });
   }, []);
 
