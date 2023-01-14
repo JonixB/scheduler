7ea032ff9d9357665275b9c896ccd6ad4148efa1
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-13 16:47:41 -0800

    change state to an object

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,8 +45,17 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));
+  }
 
   useEffect(() => {
     const URL = `api/days`;
@@ -66,8 +75,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
