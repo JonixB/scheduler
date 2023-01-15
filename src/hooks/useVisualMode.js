a2f2225d79e19862b191ff064e4d3340ad8d29b1
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-15 15:40:36 -0800

    refactor transition and back functions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
+  function transition(newMode, replace = false) {
     setMode(newMode);
+    if (replace) {
+      return setHistory(prev => [...prev.slice(0, -1), newMode]);
+    }
+    setHistory(prev => [...prev, newMode])
   };
 
-  return { mode, transition };
+  function back() {
+    const copyHistory = [...history];
+    copyHistory.pop(mode)
+    setHistory(() => copyHistory);
+    if (history.length > 1) {
+      setMode(() => copyHistory[(copyHistory.length - 1)]);
+    }
+  };
+  return { mode, transition, back };
 }
