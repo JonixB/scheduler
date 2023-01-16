2545cc2618267baf96d21629c84a349389eac7ad
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-15 17:01:54 -0800

    create getInterviewersForDay selector and a test for it

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -24,3 +24,23 @@ export function getInterview(state, interview) {
   }
   return { student: interview.student, interviewer: state.interviewers[interview.interviewer] }
 }
+
+export function getInterviewersForDay(state, day) {
+
+  if (!state.days.find(stateDay => stateDay.name === day)) {
+    return [];
+  }
+
+  const filteredDay = state.days.find(stateDay => stateDay.name === day)
+
+  if (state.days.length < 1) {
+    return [];
+  }
+
+  const result = filteredDay.interviewers.map((interviewer) => {
+    if (interviewer in state.interviewers) {
+      return state.interviewers[interviewer];
+    }
+  });
+  return result;
+};
