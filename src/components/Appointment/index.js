c46aa9fbf6ccab2761bf59c18918de565bc6cd81
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-15 16:11:54 -0800

    create a conditional to show form when transition is on CREATE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import "components/Appointment/styles.scss";
-import Header from "components/Appointment/Header";
-import Show from "components/Appointment/Show";
-import Empty from "components/Appointment/Empty";
+import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -15,11 +17,17 @@ export default function Appointment(props) {
 
   return (
     <article className="appointment">
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={back}
         />
       )}
     </article>
