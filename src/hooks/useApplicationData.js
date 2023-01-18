4648609b31b829b87095c41107e7b04dc1cf159c
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-17 19:45:31 -0800

    create a mock for axios and a test for the get request

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -79,7 +79,9 @@ export default function useApplicationData() {
       axios.get('api/interviewers')
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    });
+      
+    })
+    .catch((err) => console.log(err))
   }, []); // empty array for useEffect dependency. Ensures the promise is only done on-load/once 
 
   return {
